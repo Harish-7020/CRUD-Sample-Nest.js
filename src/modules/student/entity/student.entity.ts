f4cad1930efb19a3cd7818e085f94729a61d0d60
Harish-7020 <narayananharish011@gmail.com>
2024-07-10 12:17:52 +0530

    removed Commented codes

--- src/modules/student/entity/student.entity.ts
+++ src/modules/student/entity/student.entity.ts
@@ -31,36 +31,3 @@ export class Students {
   
 }
 
-// import { Subjects } from "src/modules/subject/entity/subject.entity";
-// import { Column, Entity, JoinTable, ManyToMany, PrimaryColumn } from "typeorm";
-
-
-// @Entity()
-// export class Students {
-//     @PrimaryColumn()
-//     StudentID: number;
-
-//     @Column()
-//     FirstName: string;
-
-//     @Column()
-//     LastName: string;
-
-//     @Column()
-//     Class: string;
-
-//     @Column()
-//     City: string;
-
-//     @Column()
-//     DOB: Date;
-
-//     @Column()
-//     Grade: string;
-
-//     @ManyToMany(() => Subjects)
-//     @JoinTable()
-//     subjects: Subjects[];
-  
-    
-// }
--- src/modules/student/student.controller.ts
+++ src/modules/student/student.controller.ts
@@ -28,49 +28,9 @@ export class StudentsController {
     return this.studentsService.update(id, updateStudentDto);
   }
 
-  
+
   @Delete(':id')
   async remove(@Param('id') id: number) {
     return this.studentsService.remove(id);
   }
 }
-
-
-// import { Controller, Get, Post, Put, Delete, Param, Body } from '@nestjs/common';
-// import { StudentsService } from './student.service';
-// import { Students } from './entity/student.entity';
-// import { Subjects } from '../subject/entity/subject.entity';
-
-// @Controller('Students')
-// export class StudentsController {
-//   constructor(private readonly StudentsService: StudentsService) {}
-
-//   @Get()
-//   async findAll(): Promise<Students[]> {
-//     return this.StudentsService.findAll();
-//   }
-
-//   @Get(':id')
-//   async findOne(@Param('id') id: string): Promise<Students> {
-//     return this.StudentsService.findOne(+id);
-//   }
-
-//   @Post()
-//   async create(@Body() studentData: Partial<Students>): Promise<Students> {
-//     return this.StudentsService.create(studentData);
-//   }
-  
-//   @Put(':id')
-//   async update(@Param('id') id: string, @Body() studentData: Partial<Students>): Promise<Students> {
-//     return this.StudentsService.update(+id, studentData);
-//   }
-
-//   @Delete(':id')
-//   async delete(@Param('id') id: string): Promise<void> {
-//     return this.StudentsService.delete(+id);
-//   }
-//   @Get(':id/subjects')
-//   async findSubjectsByStudentID(@Param('id') id: string): Promise<Subjects[]> {
-//     return this.StudentsService.findSubjectsByStudentID(+id);
-//   }
-// }
